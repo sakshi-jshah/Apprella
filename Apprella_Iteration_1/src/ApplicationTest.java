a521e21276cfe3467c9b207dadc8da271838f00b
Lucia Beickman <beickmle@miamioh.edu>
2021-04-08 16:46:37 -0400

    JUnitTest

---
+++ Apprella_Iteration_1/src/ApplicationTest.java
@@ -0,0 +1,142 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void testApplicationStringStringStringStringStringStringIntDoubleDoubleDoubleBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplicationApplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplicationRandomAccessFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAppName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAppName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAppUID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAppUID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLanguage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLanguage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPublishDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPublishDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecommendAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRecommendAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsCompatibility() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCompatibility() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Apprella_Iteration_1/src/inputOutputGUITest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class inputOutputGUITest {
+
+	@Test
+	public void testPrintToResults() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchResults() {
+		fail("Not yet implemented");
+	}
+
+}
