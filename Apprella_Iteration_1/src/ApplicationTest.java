35ed70d1a0f896718fd5e5687f392bb7eb61cfd0
Lucia Beickman <beickmle@miamioh.edu>
2021-04-08 21:48:53 -0400

    ApplicationTestUnit

--- Apprella_Iteration_1/src/ApplicationTest.java
+++ Apprella_Iteration_1/src/ApplicationTest.java
@@ -6,23 +6,38 @@ class ApplicationTest {
 
 	@Test
 	void testApplicationStringStringStringStringStringStringIntDoubleDoubleDoubleBoolean() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {
+		};
+		assertEquals("Among Us", a.getAppName());
+		assertEquals("1234", a.getAppUID());
+		assertEquals("Game", a.getCategory());
+		assertEquals("1.0", a.getVersion());
+		assertEquals("English", a.getLanguage());
+		assertEquals("May 15, 2021", a.getPublishDate());
+		assertEquals(12, a.getRecommendAge());
+		assertEquals(0.0, a.getPrice());
+		assertEquals(5.0, a.getRating());
+		assertEquals(3.1, a.getSize());
+		assertFalse(a.isCompatibility());
 	}
 
 	@Test
 	void testApplicationApplication() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		Application b = new Application(a) {};
+		assertEquals("Among Us", b.getAppName());
+		assertEquals("1234", b.getAppUID());
+		assertEquals("Game", b.getCategory());
+		assertEquals("1.0", b.getVersion());
+		assertEquals("English", b.getLanguage());
+		assertEquals("May 15, 2021", b.getPublishDate());
+		assertEquals(12, b.getRecommendAge());
+		assertEquals(0.0, b.getPrice());
+		assertEquals(5.0, b.getRating());
+		assertEquals(3.1, b.getSize());
+		assertFalse(b.isCompatibility());
 	}
 
-	@Test
-	void testApplication() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testApplicationRandomAccessFile() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testToString() {
@@ -31,112 +46,145 @@ class ApplicationTest {
 
 	@Test
 	void testGetAppName() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("Among Us", a.getAppName());
 	}
 
 	@Test
 	void testSetAppName() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setAppName("Minecraft");
+		assertEquals("Minecraft", a.getAppName());
 	}
 
 	@Test
 	void testGetAppUID() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("1234", a.getAppUID());
 	}
 
 	@Test
 	void testSetAppUID() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setAppUID("12345");
+		assertEquals("12345", a.getAppUID());
 	}
 
 	@Test
 	void testGetCategory() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("Game", a.getCategory());
 	}
 
 	@Test
 	void testSetCategory() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setCategory("Action");
+		assertEquals("Action", a.getCategory());
 	}
 
 	@Test
 	void testGetVersion() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("1.0", a.getVersion());
 	}
 
 	@Test
 	void testSetVersion() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setVersion("2.5");
+		assertEquals("2.5", a.getVersion());
 	}
 
 	@Test
 	void testGetLanguage() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("English", a.getLanguage());
 	}
 
 	@Test
 	void testSetLanguage() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setLanguage("French");
+		assertEquals("French", a.getLanguage());
 	}
 
 	@Test
 	void testGetPublishDate() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals("May 15, 2021", a.getPublishDate());
 	}
 
 	@Test
 	void testSetPublishDate() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setPublishDate("April 1, 2021");
+		assertEquals("April 1, 2021", a.getPublishDate());
 	}
 
 	@Test
 	void testGetRecommendAge() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals(12, a.getRecommendAge());
 	}
 
 	@Test
 	void testSetRecommendAge() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setRecommendAge(5);
+		assertEquals(5, a.getRecommendAge());
 	}
 
 	@Test
 	void testGetPrice() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals(0.0, a.getPrice());
 	}
 
 	@Test
 	void testSetPrice() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setPrice(2.99);
+		assertEquals(2.99, a.getPrice());
 	}
 
 	@Test
 	void testGetRating() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals(5.0, a.getRating());
 	}
 
 	@Test
 	void testSetRating() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setRating(0.0);
+		assertEquals(0.0, a.getRating());
 	}
 
 	@Test
 	void testGetSize() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertEquals(3.1, a.getSize());
 	}
 
 	@Test
 	void testSetSize() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setSize(5.9);
+		assertEquals(5.9, a.getSize());
 	}
 
 	@Test
 	void testIsCompatibility() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		assertFalse(a.isCompatibility());
 	}
 
 	@Test
 	void testSetCompatibility() {
-		fail("Not yet implemented");
+		Application a = new Application("Among Us", "1234", "Game", "1.0", "English", "May 15, 2021", 12, 0.0, 5.0, 3.1, false) {};
+		a.setCompatibility(true);
+		assertTrue(a.isCompatibility());
 	}
 
 }
